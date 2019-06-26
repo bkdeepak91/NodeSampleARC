1bd8af2535bcc29bf9c54d6a36a9625d074788d6
Deepak Balakrishnan <deepak.bk@lifetrenz.com>
2019-06-26 12:14:55 +0530

    Added try catch

--- controllers/Authorization.js
+++ controllers/Authorization.js
@@ -2,46 +2,50 @@ var user = require('../repository/User');
 var userType = require('../models/UserType');
 
 var authControl = {
-    isAuthenticated : async(req, res, next) => {
+    isAuthenticated: async (req, res, next) => {
         var tokenId = req.header('userToken');
-
-        var userDetails = await user.getUserByToken(tokenId);
-        var apiResult = {};
-        // If token is invalid
-        if(userDetails == undefined){
-            apiResult.meta = {
-                success : false,
-                error : "Unauthorized!"
-            };
-            apiResult.data = [];
-            res.status(401).json(apiResult);
-        }
-        // If token is valid and
-        else {
-            // No Access
-            if(userDetails.type == userType.NO_ACCESS){
+        try {
+            var userDetails = await user.getUserByToken(tokenId);
+            var apiResult = {};
+            // If token is invalid
+            if (userDetails == undefined) {
                 apiResult.meta = {
-                    success : false,
-                    error : "Unauthorized!"
+                    success: false,
+                    error: "Unauthorized!"
                 };
                 apiResult.data = [];
                 res.status(401).json(apiResult);
             }
-            // No write access
-            if(userDetails.type == userType.READ && req.method != 'GET'){
-                apiResult.meta = {
-                    success : false,
-                    error : "No Write Access!"
-                };
-                apiResult.data = [];
-                res.status(401).json(apiResult);
+            // If token is valid and
+            else {
+                // No Access
+                if (userDetails.type == userType.NO_ACCESS) {
+                    apiResult.meta = {
+                        success: false,
+                        error: "Unauthorized!"
+                    };
+                    apiResult.data = [];
+                    res.status(401).json(apiResult);
+                }
+                // No write access
+                if (userDetails.type == userType.READ && req.method != 'GET') {
+                    apiResult.meta = {
+                        success: false,
+                        error: "No Write Access!"
+                    };
+                    apiResult.data = [];
+                    res.status(401).json(apiResult);
+                }
+                // Athourized person
+                req.user = {
+                    id: userDetails.id,
+                    type: userDetails.type
+                }
+                next();
             }
-            // Athourized person
-            req.user = {
-                id: userDetails.id,
-                type: userDetails.type
-            }
-            next();
+        } catch (e) {
+            //Error Handling
+            logger.error('Authorization controller caught exception: ', e);
         }
     },
 }
