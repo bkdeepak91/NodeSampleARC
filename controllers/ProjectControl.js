d40b42dade49ab35acd66673912fa794e88b6d79
shaileshkumark <k.shailesh94@gmail.com>
2019-07-02 11:23:09 +0530

    converted to async and await

--- controllers/ProjectControl.js
+++ controllers/ProjectControl.js
@@ -1,51 +1,61 @@
-var project  = require('../repository/Project');
+var project = require('../repository/Project');
+var logger = require('../models/errorLogger.js');
+
 
 var projectControl = {
 
     //Create Project
-    createProject : (req, res) => {
+    createProject: async (req, res) => {
         var Project = {
             name: req.body.name,
             city: req.body.city,
             createdBy: req.user.id
         };
 
-        project.createProject(Project, (err, projectData) => {
+        try {
+            var projectData = await project.createProject(Project);
+
+            console.log('Proj Data==>', projectData);
+
             var apiResult = {};
-            if(err){
+            if (projectData == undefined) {
                 apiResult.metaData = {
-                    success : false,
-                    error : err
+                    success: false,
+                    error: "error"
                 };
 
                 apiResult.data = [];
                 res.json(apiResult);
             } else {
                 apiResult.metaData = {
-                    success : true,
-                    rows : 1
+                    success: true,
+                    rows: 1
                 };
-
                 apiResult.data = projectData;
                 res.json(apiResult);
             }
-        });
+        } catch (e) {
+            logger.error('Project controller caught exception: ', e);
+        }
     },
 
+
     // Update Project
-    updateProject: (req, res) => {
+    updateProject: async (req, res) => {
         var Project = {
             id: req.params.projectId,
             name: req.body.updatedName,
             city: req.body.updatedCity
         };
 
-        project.updateProject(Project, (err, projectData) => {
+        try {
+
+            var projectData = await project.updateProject(Project);
             var apiResult = {};
-            if (err) {
+            if (projectData == undefined) {
                 apiResult.metaData = {
                     success: false,
-                    error: err
+                    error: 'err'
                 };
 
                 apiResult.data = [];
@@ -59,11 +69,17 @@ var projectControl = {
                 apiResult.data = projectData;
                 res.json(apiResult);
             }
-        });
+        }
+        catch (e) {
+
+            logger.error('Project controller caught exception: ', e);
+
+        }
     },
 
+
     // Share Project
-    shareProject: (req, res) => {
+    shareProject: async (req, res) => {
         var Project = {
             projectId: req.body.projectId,
             userId: req.body.userId,
@@ -71,12 +87,14 @@ var projectControl = {
             createdBy: req.user.id,
             validity: req.body.validity
         };
-        project.shareProject(Project, (err, projectData) => {
+
+        try {
+            var projectData = await project.shareProject(Project);
             var apiResult = {};
-            if (err) {
+            if (projectData == undefined) {
                 apiResult.metaData = {
                     success: false,
-                    error: err
+                    error: 'error'
                 };
 
                 apiResult.data = [];
@@ -90,49 +108,62 @@ var projectControl = {
                 apiResult.data = projectData;
                 res.json(apiResult);
             }
-        });
+        }
+        catch (e) {
+
+            logger.error('Project controller caught exception: ', e);
+
+        }
     },
 
     // Get Project  
-    getProject: (req, res) => {
+    getProject: async (req, res) => {
 
         var projectId = req.params.projectId;
-        // Getting the project details
-        project.getProject(projectId, (err, projectData) => {
-            var apiResult = {};
 
-            if (err) {
+        // Getting the project details
+        try {
+            var projectData = await project.getProject(projectId)
+            var apiResult = {};
+            if (projectData == undefined) {
                 apiResult.metaData = {
                     success: false,
-                    error: err
+                    error: 'error'
                 };
 
                 apiResult.data = [];
                 res.json(apiResult);
             } else {
                 //Getting the user details
-                project.getUsersByProject(projectId, (erruser, userData) => {
-                    if (erruser) {
-                        apiResult.metaData = {
-                            success: false,
-                            error: erruser
-                        };
-                        apiResult.data = [];
-                        res.json(apiResult);
-                    }
-                    else {
-                        apiResult.metaData = {
-                            success: true,
-                            rows: 1
-                        };
-                        projectData.users = userData;
-                        apiResult.data = projectData;
-                        res.json(apiResult);
-                    }
-                })
+                var projectData = await project.getUsersByProject(projectId);
+                if (projectData == undefined) {
+                    apiResult.metaData = {
+                        success: false,
+                        error: 'erruser'
+                    };
+                    apiResult.data = [];
+                    res.json(apiResult);
+                }
+                else {
+                    apiResult.metaData = {
+                        success: true,
+                        rows: 1
+                    };
+                    projectData.users = userData;
+                    apiResult.data = projectData;
+                    res.json(apiResult);
+                }
+
             }
-        });
+        }
+        catch (e) {
+
+            logger.error('Project controller caught exception: ', e);
+
+        }
     }
 };
+
+
 
 module.exports = projectControl;
--- repository/Project.js
+++ repository/Project.js
@@ -1,56 +1,64 @@
 var db = require('./Connection');
-var logger = require('../models/errorLogger');
+var logger = require('../models/errorLogger.js');
+
 
 var Project = {
-    createProject: (project, done) => {
-        return db.query(
-            "INSERT INTO pr_projects (name, city,  created_by) values(?,?,?);",
-            [project.name, project.city, project.createdBy],
-            (error, projectData, fields) => {
-                if(error) logger.error('DB query Exception: ' , error);
-                done(error, projectData);
-            });
+    async createProject(project) {
+        try {
+            projectData = await db.query(
+                "INSERT INTO pr_projects (name, city,  created_by) values(?,?,?);",
+                [project.name, project.city, project.createdBy]);
+            return projectData;
+        } catch (e) {
+            logger.error('Project repository caught exception:', e);
+        }
     },
 
-    updateProject: (project, done) => {
-        return db.query(
-            "UPDATE pr_projects SET name = ? , city = ? where id=?;",
-            [project.name, project.city, project.id],
-            (error, projectData, fields) => {
-                if(error) logger.error('DB query Exception: ' , error);
-                done(error, projectData);
-            });
+    async updateProject(project) {
+        try {
+            projectData = await db.query(
+                "UPDATE pr_projects SET name = ? , city = ? where id=?;",
+                [project.name, project.city, project.id]);
+            return projectData[0];
+        } catch (e) {
+            logger.error('Project repository caught exception:', e);
+        }
     },
 
-    shareProject: (project, done) => {
-        return db.query(
-            "INSERT INTO pr_project_user (project_id, user_id, mode,created_by,validity)values(?,?,?,?,?);",
-            [project.projectId, project.userId, project.mode, project.createdBy, project.validity],
-            (error, projectData, fields) => {
-                if(error) logger.error('DB query Exception: ' , error);
-                done(error, projectData);
-            });
+    async shareProject(project) {
+        try {
+            projectData = await db.query(
+                "INSERT INTO pr_project_user (project_id, user_id, mode,created_by,validity)values(?,?,?,?,?);",
+                [project.projectId, project.userId, project.mode, project.createdBy, project.validity]);
+            return projectData;
+        } catch (e) {
+            logger.error('Project repository caught exception:', e);
+        }
 
     },
 
-    getProject: (projectId, done) => {
-        return db.query(
-            "select id as project_id, name, city, created_by from pr_projects WHERE id = ?;",
-            [projectId],
-            (error, projectData, fields) => {
-                if(error) logger.error('DB query Exception: ' , error);
-                done(error, projectData[0]);
-            });
+    async getProject(projectId) {
+        try {
+            projectData = await db.query(
+                "select id as project_id, name, city, created_by from pr_projects WHERE id = ?;",
+                [projectId]);
+            return projectData[0];
+        } catch (e) {
+            logger.error('Project repository caught exception:', e);
+        }
+
     },
 
-    getUsersByProject: (projectId, done) => {
-        return db.query(
-            "select u.id as userId, u.name as userName, u.email as userEmail, u.type as accessType from pr_project_user pu, ac_users u WHERE pu.user_id = u.id and pu.project_id = ?;",
-            [projectId],
-            (error, userData, fields) => {
-                if(error) logger.error('DB query Exception: ' , error);
-                done(error, userData);
-            });
+    async getUsersByProject(projectId) {
+        try {
+            userData = await db.query(
+                "select u.id as userId, u.name as userName, u.email as userEmail, u.type as accessType from pr_project_user pu, ac_users u WHERE pu.user_id = u.id and pu.project_id = ?;",
+                [projectId]);
+            return userData;
+        } catch (e) {
+            logger.error('Project repository caught exception:', e);
+        }
+
     }
 };
 
